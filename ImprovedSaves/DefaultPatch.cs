eba20ee805a4986fcd53313ac31418b5dedb3b3c
HexiDave <hexidave@gmail.com>
2018-02-16 03:37:27 -0500

    Added a 5min autosave mod

---
+++ ImprovedSaves/DefaultPatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using BootstrapLib;
+
+namespace ImprovedSaves
+{
+    class DefaultPatch : IPatch
+    {
+        public void InitializePatch(ModuleDefMD module)
+        {
+            Importer importer = new Importer(module);
+
+            // Import and store the patching methods
+            var performSaveMethod = importer.Import(typeof(SaveHelper).GetMethod("PerformSave"));
+            var startSaveInvokerMethod = importer.Import(typeof(SaveHelper).GetMethod("StartSaveInvoker"));
+
+            var playerType = module.Find("Player", false);
+
+            var autoSaveMethod = new MethodDefUser(
+                "Autosave",
+                MethodSig.CreateInstance(module.CorLibTypes.Void),
+                MethodImplAttributes.IL | MethodImplAttributes.Managed,
+                MethodAttributes.HideBySig | MethodAttributes.Public | MethodAttributes.ReuseSlot
+            );
+
+            playerType.Methods.Add(autoSaveMethod);
+
+            var body = autoSaveMethod.Body = new CilBody();
+
+            body.Instructions.Add(OpCodes.Call.ToInstruction(performSaveMethod));
+            body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+
+            var awakeMethod = playerType.FindMethod("Awake");
+            var instructions = awakeMethod.Body.Instructions;
+
+            instructions.Insert(instructions.Count - 1, OpCodes.Ldarg_0.ToInstruction());
+            instructions.Insert(instructions.Count - 1, OpCodes.Call.ToInstruction(startSaveInvokerMethod));
+        }
+    }
+}
---
+++ ImprovedSaves/SaveHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImprovedSaves
+{
+    public static class SaveHelper
+    {
+        static readonly float AutosaveTime = 5f * 60f;
+        public static void PerformSave()
+        {
+            ErrorMessage.AddMessage("Saving...");
+
+            if (IngameMenu.main.gameObject.activeSelf)
+            {
+                ErrorMessage.AddMessage("Unable to autosave - menu was open.");
+                return;
+            }
+
+            IngameMenu.main.SaveGame();
+            IngameMenu.main.OnDeselect();
+        }
+
+        public static void StartSaveInvoker(Player player)
+        {
+            player.InvokeRepeating("Autosave", AutosaveTime, AutosaveTime);
+            ErrorMessage.AddMessage("Started invoker");
+        }
+    }
+}
