cab900119c683820b4e5e765d075b0257f80c52f
HexiDave <hexidave@gmail.com>
2018-02-18 07:41:04 -0500

    Added nuclear power to the CyclopsPower mod - can now charge the Cyclops with a nuclear reactor

--- CyclopsPower/DefaultPatch.cs
+++ CyclopsPower/DefaultPatch.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
@@ -12,6 +11,12 @@ namespace CyclopsPower
     class DefaultPatch : IPatch
     {
         public void InitializePatch(ModuleDefMD module)
+        {
+            PatchPowerIndicator(module);
+            PatchNuclearUpgrade(module);
+        }
+
+        private void PatchPowerIndicator(ModuleDefMD module)
         {
             var importer = new Importer(module);
             var patchIsPowerEnabled = importer.Import(typeof(PowerIndicatorHelper).GetMethod("IsPowerEnabled"));
@@ -32,6 +37,98 @@ namespace CyclopsPower
             })
             {
                 body.Instructions.Add(instruction);
+            }
+        }
+
+        private void PatchNuclearUpgrade(ModuleDefMD module)
+        {
+            PatchNuclearUpgrade_EquipmentType(module);
+            PatchNuclearUpgrade_CraftData(module);
+            PatchNuclearUpgrade_SubRoot(module);
+        }
+
+        private void PatchNuclearUpgrade_EquipmentType(ModuleDefMD module)
+        {
+            var equipmentType = module.Find("EquipmentType", false);
+
+            FieldDef fieldToAdd =
+                new FieldDefUser("NuclearReactorOrCyclopsModule", new FieldSig(new ValueTypeSig(equipmentType)))
+                {
+                    Constant = module.UpdateRowId(new ConstantUser(NuclearUpgradeHelper.NuclearReactorOrCyclopsModule,
+                        equipmentType.GetEnumUnderlyingType().ElementType)),
+                    Attributes = FieldAttributes.Literal | FieldAttributes.Static | FieldAttributes.HasDefault |
+                                 FieldAttributes.Public
+                };
+
+            equipmentType.Fields.Add(fieldToAdd);
+        }
+
+        private void PatchNuclearUpgrade_CraftData(ModuleDefMD module)
+        {
+            var craftDataType = module.Find("CraftData", false);
+            var equipmentTypesField = craftDataType.FindField("equipmentTypes");
+            equipmentTypesField.Attributes = FieldAttributes.Public | FieldAttributes.Static;
+
+            var equipmentType = module.Find("Equipment", false);
+            var isCompatibleMethod = equipmentType.FindMethod("IsCompatible");
+
+            var importer = new Importer(module);
+            var isCompatiblePatchMethod = importer.Import(typeof(NuclearUpgradeHelper).GetMethod("IsCompatible"));
+
+            var body = isCompatibleMethod.Body = new CilBody();
+            body.Instructions.Add(OpCodes.Ldarg_0.ToInstruction());
+            body.Instructions.Add(OpCodes.Ldarg_1.ToInstruction());
+            body.Instructions.Add(OpCodes.Call.ToInstruction(isCompatiblePatchMethod));
+            body.Instructions.Add(OpCodes.Ret.ToInstruction());
+        }
+
+        private void PatchNuclearUpgrade_SubRoot(ModuleDefMD module)
+        {
+            var subRootType = module.Find("SubRoot", false);
+
+            var fieldToAdd = new FieldDefUser("nuclearUpgrade",
+                new FieldSig(new ValueTypeSig(module.CorLibTypes.Int32.TypeDefOrRef)))
+            {
+                Attributes = FieldAttributes.Public
+            };
+
+            subRootType.Fields.Add(fieldToAdd);
+
+            var setCyclopsUpgradesMethod = subRootType.FindMethod("SetCyclopsUpgrades");
+
+            var importer = new Importer(module);
+            var setCyclopsUpgradesPatchMethod =
+                importer.Import(typeof(NuclearUpgradeHelper).GetMethod("SetCyclopsUpgrades"));
+
+            var body = setCyclopsUpgradesMethod.Body = new CilBody();
+
+            body.Instructions.Add(OpCodes.Ldarg_0.ToInstruction());
+            body.Instructions.Add(OpCodes.Call.ToInstruction(setCyclopsUpgradesPatchMethod));
+            body.Instructions.Add(OpCodes.Ret.ToInstruction());
+
+            PatchNuclearUpgrade_SubRoot_Update(module, subRootType, importer);
+        }
+
+        private void PatchNuclearUpgrade_SubRoot_Update(ModuleDefMD module, TypeDef subRootType, Importer importer)
+        {
+            subRootType.FindField("slotNames").Attributes = FieldAttributes.Public | FieldAttributes.Static | FieldAttributes.InitOnly;
+            //subRootType.FindField("live").Attributes = FieldAttributes.Public;
+
+            var updateMethod = subRootType.FindMethod("Update");
+
+            var updateNuclearRechargePatchMethod = importer.Import(typeof(NuclearUpgradeHelper).GetMethod("UpdateNuclearRecharge"));
+
+            var instructions = updateMethod.Body.Instructions;
+
+            var instructionsToAdd = new[]
+            {
+                OpCodes.Ldarg_0.ToInstruction(),
+                OpCodes.Call.ToInstruction(updateNuclearRechargePatchMethod)
+            };
+
+            foreach (var instruction in instructionsToAdd)
+            {
+                instructions.Insert(instructions.Count - 1, instruction);
             }
         }
     }
---
+++ CyclopsPower/NuclearUpgradeHelper.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using BootstrapLib;
+using UnityEngine;
+
+namespace CyclopsPower
+{
+    public class NuclearUpgradeHelper : IMod
+    {
+        private static readonly FieldInfo EquipmentTypesField;
+
+        private static readonly FieldInfo SlotNamesField;
+        private static readonly FieldInfo LiveField;
+        private static readonly FieldInfo NuclearUpgradeField;
+        public static readonly int NuclearReactorOrCyclopsModule = 100;
+
+        static NuclearUpgradeHelper()
+        {
+            var craftDataType = typeof(CraftData);
+
+            EquipmentTypesField = craftDataType.GetField("equipmentTypes", BindingFlags.Static | BindingFlags.Public);
+
+            var subRootType = typeof(SubRoot);
+            SlotNamesField = subRootType.GetField("slotNames", BindingFlags.Static | BindingFlags.Public);
+            LiveField = subRootType.GetField("live", BindingFlags.NonPublic | BindingFlags.Instance);
+            NuclearUpgradeField =
+                subRootType.GetField("nuclearUpgrade", BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        public static void ChangeNuclearRodEquipmentTypes()
+        {
+            var equipmentTypes = EquipmentTypesField.GetValue(null) as Dictionary<TechType, EquipmentType>;
+            equipmentTypes[TechType.ReactorRod] = (EquipmentType) NuclearReactorOrCyclopsModule;
+        }
+
+        public static bool IsCompatible(EquipmentType itemType, EquipmentType slotType)
+        {
+            return
+                itemType == slotType ||
+                (itemType == EquipmentType.VehicleModule &&
+                 (slotType == EquipmentType.SeamothModule || slotType == EquipmentType.ExosuitModule)) ||
+                (itemType == (EquipmentType) NuclearReactorOrCyclopsModule &&
+                 (slotType == EquipmentType.NuclearReactor || slotType == EquipmentType.CyclopsModule));
+        }
+
+        public static void SetCyclopsUpgrades(SubRoot subRoot)
+        {
+            
+            var live = LiveField.GetValue(subRoot) as LiveMixin;
+            if (subRoot.upgradeConsole == null || !live.IsAlive()) return;
+
+            subRoot.shieldUpgrade = false;
+            subRoot.sonarUpgrade = false;
+            subRoot.vehicleRepairUpgrade = false;
+            subRoot.decoyTubeSizeIncreaseUpgrade = false;
+            subRoot.thermalReactorUpgrade = false;
+            var nuclearUpgrades = 0;
+
+            var refreshArray = new TechType[6];
+            var modules = subRoot.upgradeConsole.modules;
+            var slotNames = SlotNamesField.GetValue(null) as string[];
+
+            for (var i = 0; i < 6; i++)
+            {
+                var techTypeInSlot = modules.GetTechTypeInSlot(slotNames[i]);
+
+                switch (techTypeInSlot)
+                {
+                    case TechType.CyclopsShieldModule:
+                        subRoot.shieldUpgrade = true;
+                        break;
+                    case TechType.CyclopsSonarModule:
+                        subRoot.sonarUpgrade = true;
+                        break;
+                    case TechType.CyclopsSeamothRepairModule:
+                        subRoot.vehicleRepairUpgrade = true;
+                        break;
+                    case TechType.CyclopsDecoyModule:
+                        subRoot.decoyTubeSizeIncreaseUpgrade = true;
+                        break;
+                    case TechType.CyclopsThermalReactorModule:
+                        subRoot.thermalReactorUpgrade = true;
+                        break;
+                    case TechType.ReactorRod:
+                        nuclearUpgrades++;
+                        break;
+                }
+
+                refreshArray[i] = techTypeInSlot;
+            }
+
+            NuclearUpgradeField.SetValue(subRoot, nuclearUpgrades);
+
+            if (subRoot.slotModSFX != null)
+            {
+                subRoot.slotModSFX.Play();
+            }
+
+            subRoot.BroadcastMessage("RefreshUpgradeConsoleIcons", refreshArray, SendMessageOptions.RequireReceiver);
+        }
+
+        public static void UpdateNuclearRecharge(SubRoot subRoot)
+        {
+            var nuclearUpgrades = (int)NuclearUpgradeField.GetValue(subRoot);
+            if (nuclearUpgrades > 0)
+            {
+                const float powerPerUpgrade = 1.5f;
+                subRoot.powerRelay.AddEnergy(Time.deltaTime * powerPerUpgrade * (float) nuclearUpgrades,
+                    out var outAmount);
+            }
+        }
+
+        public void InitializeMod()
+        {
+            ChangeNuclearRodEquipmentTypes();
+        }
+    }
+}
